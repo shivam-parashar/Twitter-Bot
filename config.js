04e806eed62b19fcf6fa4bb3c7120f1238aae520
shivam-parashar <mailtoageek@gmail.com>
2018-07-28 19:23:45 +0530

    Uploading the config file

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+   consumer_key:          '3Z0glCnpN2iJr7GbvR5yJsdCv'
+  ,consumer_secret:      'ZQOK3qX0w2j0m2vIyp7ZkhDvM1x2ciisO42eXkULQVmJi1kqi6'
+  ,access_token:         '874001386051993600-CrjBx4QRMQLjmqM5SETvXpJPEnF9Ib3'
+  ,access_token_secret:  'uAKlQ0VrywAh4OzCl705Qwoo4bA6y3VUCxjFyU59qwjM2'
+  }
+
+
